f0072f0d9b380378e24078b2dc170d4cd38c07bd
ChoJinmok <jjmok1127@gmail.com>
2022-03-30 16:27:27 +0900

    프로젝트 초기 설정

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hdelooooooooo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
